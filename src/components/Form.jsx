eabe31b8fb81be4619fd927de9d10c5c30086108
Lucasteisouza <lucasteisouza@gmail.com>
2022-10-26 15:37:51 -0300

    Requisito 2 atendido, linter com problema esperado

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,84 +1,142 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Form extends Component {
   render() {
+    const { cardName,
+      cardDescription,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+      cardImage,
+      cardRare,
+      cardTrunfo,
+      hasTrunfo,
+      isSaveButtonDisabled,
+      onInputChange,
+      onSaveButtonClick } = this.props;
     return (
-      <form>
-        <label htmlFor="name-input">
-          Nome da carta:
-          <input
-            type="text"
-            name="name-input"
-            id="name-input"
-            data-testid="name-input"
-          />
-        </label>
-        Descrição da carta:
-        <label htmlFor="description-input">
-          <textarea
-            name="description-input"
-            id="description-input"
-            cols="30"
-            rows="10"
-            data-testid="description-input"
-          />
-        </label>
-        <label htmlFor="attr1-input">
-          Placeholder attr1:
-          <input
-            type="number"
-            name="attr1-input"
-            id="attr1-input"
-            data-testid="attr1-input"
-          />
-        </label>
-        <label htmlFor="attr2-input">
-          Placeholder attr2:
-          <input
-            type="number"
-            name="attr2-input"
-            id="attr2-input"
-            data-testid="attr2-input"
-          />
-        </label>
-        <label htmlFor="attr3-input">
-          Placeholder attr3:
-          <input
-            type="number"
-            name="attr3-input"
-            id="attr3-input"
-            data-testid="attr3-input"
-          />
-        </label>
-        <label htmlFor="image-input">
-          Nome da carta:
-          <input
-            type="text"
-            name="image-input"
-            id="image-input"
-            data-testid="image-input"
-          />
-        </label>
-        <label htmlFor="rare-input">
-          <select name="rare-input" id="rare-input" data-testid="rare-input">
-            <option value="normal">Normal</option>
-            <option value="raro">Raro</option>
-            <option value="muito raro">Muito raro</option>
-          </select>
-        </label>
-        <label htmlFor="trunfo-input">
-          Super Trunfo?
-          <input
-            type="checkbox"
-            name="trunfo-input"
-            id="trunfo-input"
-            data-testid="trunfo-input"
-          />
-        </label>
-        <button type="submit" data-testid="save-button">Salvar</button>
-      </form>
+      <div>
+        <h2>Adicione uma nova carta</h2>
+        <form>
+          <label htmlFor="name-input">
+            Nome da carta:
+            <input
+              type="text"
+              name="name-input"
+              id="name-input"
+              data-testid="name-input"
+              value={ cardName }
+              onChange={ onInputChange }
+            />
+          </label>
+          Descrição da carta:
+          <label htmlFor="description-input">
+            <textarea
+              name="description-input"
+              id="description-input"
+              cols="30"
+              rows="10"
+              data-testid="description-input"
+              value={ cardDescription }
+              onChange={ onInputChange }
+            />
+          </label>
+          <label htmlFor="attr1-input">
+            Placeholder attr1:
+            <input
+              type="number"
+              name="attr1-input"
+              id="attr1-input"
+              data-testid="attr1-input"
+              value={ cardAttr1 }
+              onChange={ onInputChange }
+            />
+          </label>
+          <label htmlFor="attr2-input">
+            Placeholder attr2:
+            <input
+              type="number"
+              name="attr2-input"
+              id="attr2-input"
+              data-testid="attr2-input"
+              value={ cardAttr2 }
+              onChange={ onInputChange }
+            />
+          </label>
+          <label htmlFor="attr3-input">
+            Placeholder attr3:
+            <input
+              type="number"
+              name="attr3-input"
+              id="attr3-input"
+              data-testid="attr3-input"
+              value={ cardAttr3 }
+              onChange={ onInputChange }
+            />
+          </label>
+          <label htmlFor="image-input">
+            Nome da carta:
+            <input
+              type="text"
+              name="image-input"
+              id="image-input"
+              data-testid="image-input"
+              value={ cardImage }
+              onChange={ onInputChange }
+            />
+          </label>
+          <label htmlFor="rare-input">
+            <select
+              name="rare-input"
+              id="rare-input"
+              data-testid="rare-input"
+              value={ cardRare }
+              onChange={ onInputChange }
+            >
+              <option value="normal">Normal</option>
+              <option value="raro">Raro</option>
+              <option value="muito raro">Muito raro</option>
+            </select>
+          </label>
+          <label htmlFor="trunfo-input">
+            Super Trunfo?
+            <input
+              type="checkbox"
+              name="trunfo-input"
+              id="trunfo-input"
+              data-testid="trunfo-input"
+              checked={ cardTrunfo }
+              onChange={ onInputChange }
+            />
+          </label>
+          <button
+            type="submit"
+            data-testid="save-button"
+            disabled={ isSaveButtonDisabled }
+            onClick={ onSaveButtonClick }
+          >
+            Salvar
+          </button>
+        </form>
+      </div>
     );
   }
 }
+
+Form.propTypes = {
+  cardName: PropTypes.string.isRequired,
+  cardDescription: PropTypes.string.isRequired,
+  cardAttr1: PropTypes.string.isRequired,
+  cardAttr2: PropTypes.string.isRequired,
+  cardAttr3: PropTypes.string.isRequired,
+  cardImage: PropTypes.string.isRequired,
+  cardRare: PropTypes.string.isRequired,
+  cardTrunfo: PropTypes.bool.isRequired,
+  hasTrunfo: PropTypes.bool.isRequired,
+  isSaveButtonDisabled: PropTypes.bool.isRequired,
+  onInputChange: PropTypes.func.isRequired,
+  onSaveButtonClick: PropTypes.func.isRequired,
+};
 
 export default Form;
